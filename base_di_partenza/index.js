1f98a9fe116d740ed8b05784f47bf0df0ec131ed
Giulio-Marra <giuliomarramail@gmail.com>
2024-03-30 15:43:24 +0100

    addFunctionFprRandomPath

--- base_di_partenza/index.js
+++ base_di_partenza/index.js
@@ -9,3 +9,23 @@ window.addEventListener("scroll", function () {
     button.classList.remove("scrollButton");
   }
 });
+
+const getRandomPath = () => {
+  const svg = document.getElementById("animatedSVG");
+  const pathElements = svg.querySelectorAll("path");
+
+  for (let i = 0; i < pathElements.length; i++) {
+    const randomPath = Math.floor(Math.random() * pathElements.length);
+    const path = pathElements[i];
+
+    if (i === randomPath) {
+      if (path.style.display !== "none") {
+        path.style.display = "none";
+      } else {
+        path.style.display = "inline";
+      }
+    }
+  }
+};
+
+setInterval(getRandomPath, 100);
